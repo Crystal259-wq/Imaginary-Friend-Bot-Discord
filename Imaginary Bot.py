b75dcb657a0ba5d826d52d5cfc03a9fa3ddad22a
Crystal259-wq <79875683+Crystal259-wq@users.noreply.github.com>
2021-03-01 22:31:04 +0100

    Add files via upload

---
+++ Imaginary Bot.py
@@ -0,0 +1,26 @@
+# Import Discord Package
+import discord
+
+# Client
+client = discord.Client()
+
+@client.event
+# DO STUFF....
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('hello'):
+    await message.channel.send('Hello There!')
+
+  if message.content.startswith('friend'):
+   await message.channel.send('m-me?')
+
+  if message.content.startswith('crystal'):
+   await message.channel.send('WHO MENTIONED MY SCRIPTER???')
+
+
+  if message.content.startswith('*help'):
+   await message.channel.send('Still In Development')
+# Run The Client
+client.run('ODE1ODU0NjM1NzE3MzYxNjY0.YDyd1w.9iGKAQuX-B77j0zaJ3msTuuAAa4')
