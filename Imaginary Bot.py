34db7804cf16c448c66dc88d99b8348a5ef16780
Crystal259-wq <79875683+Crystal259-wq@users.noreply.github.com>
2021-03-01 22:40:02 +0100

    Update Imaginary Bot.py

--- Imaginary Bot.py
+++ Imaginary Bot.py
@@ -23,4 +23,4 @@ async def on_message(message):
   if message.content.startswith('*help'):
    await message.channel.send('Still In Development')
 # Run The Client
-client.run('ODE1ODU0NjM1NzE3MzYxNjY0.YDyd1w.ot_YyASrwc-s1tR_Ft4im-hHK7I')
+client.run('ODE1ODU0NjM1NzE3MzYxNjY0.YDyd1w.bkKeaAfC9EARTCkL9Ni66ETAAPQ')
